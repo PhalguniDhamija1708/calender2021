a97a1e9f2291ec95b044cd0fc15108f161d7f7e9
Phalguni Dhamija <phalguni.dhamija@cybergindia.local>
2021-04-19 13:33:47 +0530

    This is my calender

---
+++ script.js
@@ -0,0 +1,49 @@
+function newYear(){
+    alert("Enjoy the first day of your year!!!!");
+}
+function republicDay(){
+    alert("Pay tribute to those who sacrifice for your country");
+}
+function holi(){
+    alert("Lets fill this world with beautiful colors!!!")
+}
+function goodFriday(){
+    alert("Its a good friday....");
+}
+function ramzan(){
+    alert("Lets pray for healthy life!!!!");
+}
+function dussehra(){
+    alert("Dont cross your limits.")
+}
+function diwali()
+{
+    alert("Full of lights and diyas");
+}
+function christmas(){
+    alert("Santa clause coming on the christmas day!!!!");
+}
+function janmashtami(){
+    alert("Birth of krishna who teach us to stay calm in every situation.");
+}
+function memorialDay(){
+    alert("Lets honor the military personnel....");
+}
+function independenceDay(){
+    alert("From every mountain side. Let Freedom ring.");
+}
+function laborDay(){
+    alert("Respect each and every job.");
+}
+function newEve(){
+    alert("Lets dive into another year with enthusiasm.");
+}
+function eve(){
+    alert("Party for americans before christmas.");
+}
+function thanksGiving(){
+    alert("Be thankful");
+}
+
+
+
